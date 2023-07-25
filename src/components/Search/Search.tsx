d7bfe899b97f2b5136a5b115b75b925bd2ff1368
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-25 23:01:32 +0300

    (2)logic of fetching data of weather

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,148 +1,123 @@
-// import React, { useState, useEffect } from "react";
-// import styles from "./Search.module.scss";
-
-// const api = {
-//     key: "18b48209d1c103adfc7e19a05457ec9e",
-//     base: "https://api.openweathermap.org/data/2.5/",
-// };
-
-// export function Search() {
-//     const [search, setSearch] = useState("");
-//     const [weather, setWeather] = useState({ main: { temp: "" }, name: "" });
-
-//     const searchPressed = () => {
-//         fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-//             .then((res) => {
-//                 if (!res.ok) {
-//                     throw new Error("City not found");
-//                 }
-//                 return res.json();
-//             })
-//             .then((result) => {
-//                 console.log(result)
-//                 setWeather(result);
-//             })
-//             .catch((error) => {
-//                 console.error(error);
-//                 alert("City not found. Please enter a valid city name.");
-//             });
-//     };
-
-//     useEffect(() => {
-//         const delayDebounceFn = setTimeout(() => {
-//             if (search !== "") {
-//                 // Вызываем searchPressed только если search не пустая строка
-//                 searchPressed();
-//             }
-//         }, 500); // Задержка в 500 миллисекунд (0.5 секунды)
-
-//         return () => clearTimeout(delayDebounceFn);
-//     }, [search]);
-
-//     return (
-//         <div className={styles.container}>
-//             <div className={styles.modal}>
-//                 <input
-//                     type="text"
-//                     placeholder="Enter your city"
-//                     value={search}
-//                     onChange={(e) => setSearch(e.target.value)}
-//                 />
-//                 <div className={styles.listDays}>
-//                     <ul>
-//                         <li>
-//                             <div className={styles.card}>
-//                                 {/* <h2>Sun 25.07</h2> */}
-//                                 <img src="img/1.png" alt="" />
-//                                 <h3>{Math.ceil(Number(weather.main.temp))}°</h3>
-//                                 <h3>{weather.name}</h3>
-//                             </div>
-//                         </li>
-//                     </ul>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Search.module.scss";
 
 const api = {
-    key: "18b48209d1c103adfc7e19a05457ec9e",
-    base: "https://api.openweathermap.org/data/2.5/",
+  key: "18b48209d1c103adfc7e19a05457ec9e",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 type WeatherData = {
-    main: {
-        temp: string;
-    };
-    name: string;
-    weather: Array<{
-        icon: string;
-    }>;
+  main: {
+    temp: string;
+  };
+  name: string;
+  weather: Array<{
+    main: string;
+    icon: string;
+  }>;
 };
 
 export function Search() {
-    const [search, setSearch] = useState("");
-    const [weather, setWeather] = useState<WeatherData>({ main: { temp: "" }, name: "", weather: [] });
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState<WeatherData>({
+    main: { temp: "" },
+    name: "",
+    weather: [],
+  });
 
-    const getWeatherIconUrl = (iconCode: string) => {
-        return `http://openweathermap.org/img/wn/${iconCode}.png`;
-    }    
+  const getWeatherIconUrl = (main: string): string => {
+    if (main === "Sunny" || main === "Clear") {
+      return "img/2.png";
+    } 
+    if (main === "Clouds") {
+      return "img/3.png";
+    } 
+    if (main === "Rain" || main === "Drizzle") {
+      return "img/5.png";
+    } 
+    if (main === "Snow") {
+      return "img/7.png";
+    } 
+    if (main === "Thunderstorm") {
+      return "img/8.png";
+    } 
+    if (main === "Mist" || main === "Fog") {
+      return "img/9.png";
+    } 
+    if (main === "Smoke") {
+      return "img/10.png";
+    } 
+    if (main === "Haze") {
+      return "img/11.png";
+    } 
+    if (main === "Dust") {
+      return "img/12.png";
+    } 
+    if (main === "Sand") {
+      return "img/13.png";
+    } 
+    if (main === "Tornado") {
+      return "img/14.png";
+    } 
+    else {
+      // Возвращаем URL по умолчанию для других значений
+      return "http://openweathermap.org/img/wn/default.png";
+    }
+  };
 
-    const searchPressed = () => {
-        fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("City not found");
-                }
-                return res.json();
-            })
-            .then((result) => {
-                setWeather(result);
-            })
-            .catch((error) => {
-                console.error(error);
-                alert("City not found. Please enter a valid city name.");
-            });
-    };
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("City not found");
+        }
+        return res.json();
+      })
+      .then((result) => {
+        console.log(result)
+        setWeather(result);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("City not found. Please enter a valid city name.");
+      });
+  };
 
-    useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-            if (search !== "") {
-                // Вызываем searchPressed только если search не пустая строка
-                searchPressed();
-            }
-        }, 500); // Задержка в 500 миллисекунд (0.5 секунды)
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search !== "") {
+        // Вызываем searchPressed только если search не пустая строка
+        searchPressed();
+      }
+    }, 500); // Задержка в 500 миллисекунд (0.5 секунды)
 
-        return () => clearTimeout(delayDebounceFn);
-    }, [search]);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.modal}>
-                <input
-                    type="text"
-                    placeholder="Enter your city"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <input
+          type="text"
+          placeholder="Enter your city"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className={styles.listDays}>
+          <ul>
+            <li>
+              <div className={styles.card}>
+                <img
+                  src={getWeatherIconUrl(weather.weather?.[0]?.main)}
+                  alt=""
                 />
-                <div className={styles.listDays}>
-                    <ul>
-                        <li>
-                            <div className={styles.card}>
-                                <img src={getWeatherIconUrl(weather.weather?.[0]?.icon)} alt="" />
-                                <h3>{Math.ceil(Number(weather.main?.temp))}°</h3>
-                                <h3>{weather.name}</h3>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                <h3>{Math.ceil(Number(weather.main?.temp))}°</h3>
+                <h3>{weather.name}</h3>
+              </div>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-
