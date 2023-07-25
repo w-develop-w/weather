6b7761b1b96912e0de9e716ecbc884e828ca16a0
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-25 15:12:46 +0300

    addings images and changings Search componet

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -11,41 +11,64 @@ export function Search () {
                         
                         <li>
                             <div className={styles.card}>
-                                <h2>Tue 25.07</h2>
+                                <h2>Sun 25.07</h2>
                                 <img src="img/1.png" alt="" />
-                                <h3>33*</h3>
+                                <h3>33°</h3>
+                                <h3>London</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Mon 25.07</h2>
+                                <img src="img/2.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
                             </div>
                         </li>
 
                         <li>
                             <div className={styles.card}>
                                 <h2>Tue 25.07</h2>
-                                <img src="img/1.png" alt="" />
-                                <h3>33*</h3>
+                                <img src="img/3.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
                             </div>
                         </li>
 
                         <li>
                             <div className={styles.card}>
-                                <h2>Tue 25.07</h2>
-                                <img src="img/1.png" alt="" />
-                                <h3>33*</h3>
+                                <h2>Wen 25.07</h2>
+                                <img src="img/4.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
                             </div>
                         </li>
 
                         <li>
                             <div className={styles.card}>
-                                <h2>Tue 25.07</h2>
-                                <img src="img/1.png" alt="" />
-                                <h3>33*</h3>
+                                <h2>Thu 25.07</h2>
+                                <img src="img/5.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
                             </div>
                         </li>
 
                         <li>
                             <div className={styles.card}>
-                                <h2>Tue 25.07</h2>
-                                <img src="img/1.png" alt="" />
-                                <h3>33*</h3>
+                                <h2>Fri 25.07</h2>
+                                <img src="img/6.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Sat 25.07</h2>
+                                <img src="img/7.png" alt="" />
+                                <h3>33°</h3>
+                                <h3>London</h3>
                             </div>
                         </li>
                     </ul>
