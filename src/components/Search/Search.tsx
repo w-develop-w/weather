783314c7ee706a928d7d95473fc76ba9eb5fad88
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-25 14:26:01 +0300

    adding second part in Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,7 +6,52 @@ export function Search () {
         <div className={styles.container}>
             <div className={styles.modal}>
                 <input type="text" placeholder='Enter your city'/>
+                <div className={styles.listDays}>
+                    <ul>
+                        
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Tue 25.07</h2>
+                                <img src="img/1.png" alt="" />
+                                <h3>33*</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Tue 25.07</h2>
+                                <img src="img/1.png" alt="" />
+                                <h3>33*</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Tue 25.07</h2>
+                                <img src="img/1.png" alt="" />
+                                <h3>33*</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Tue 25.07</h2>
+                                <img src="img/1.png" alt="" />
+                                <h3>33*</h3>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className={styles.card}>
+                                <h2>Tue 25.07</h2>
+                                <img src="img/1.png" alt="" />
+                                <h3>33*</h3>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
         </div>
     )
 }
