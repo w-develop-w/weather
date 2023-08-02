f4d70a91492db7bf6b07d1a3cdb25fcbe8d5fa36
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-08-02 08:49:09 +0300

    adding README.org

--- src/api/fetchDataWeather.ts
+++ src/api/fetchDataWeather.ts
@@ -1,16 +1,17 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { WeatherData } from "../models/models";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { WeatherData } from "../types/models"
 
-const apiBaseUrl = "https://api.openweathermap.org/data/2.5/";
-const apiKey = "18b48209d1c103adfc7e19a05457ec9e";
+const apiBaseUrl = "https://api.openweathermap.org/data/2.5/"
+const apiKey = "18b48209d1c103adfc7e19a05457ec9e"
 
 export const weatherApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: apiBaseUrl }),
-  endpoints: (builder) => ({
-    getWeather: builder.query<WeatherData, string>({
-      query: (city) => `forecast?q=${city}&units=metric&APPID=${apiKey}&cnt=8`,
+    baseQuery: fetchBaseQuery({ baseUrl: apiBaseUrl }),
+    endpoints: (builder) => ({
+        getWeather: builder.query<WeatherData, string >({
+            query: (city) =>
+                `forecast?q=${city}&units=metric&APPID=${apiKey}&cnt=8`,
+        }),
     }),
-  }),
-});
+})
 
-export const { useGetWeatherQuery } = weatherApi;
+export const { useGetWeatherQuery } = weatherApi
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,83 +1,83 @@
-import React from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import styles from "./Search.module.scss";
-import { useGetWeatherQuery } from "../../api/fetchDataWeather";
-import { DataOfWeather, WeatherData } from "../../models/models";
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import styles from "./Search.module.scss"
+import { useGetWeatherQuery } from "../../api/fetchDataWeather"
+import { DataOfWeather, WeatherData } from "../../types/models"
 import { setSearch } from "../../store/dataSlice"
-import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks"
 
 export function Search() {
-    const dispatch = useAppDispatch();
-    const search = useAppSelector(state => state.dataOfWeather.search)
+    const dispatch = useAppDispatch()
+    const search = useAppSelector((state) => state.dataOfWeather.search)
 
     // console.log("dataOfWeather.search:", search);
 
     const { data, error, isFetching } = useGetWeatherQuery(search, {
         refetchOnMountOrArgChange: search !== "",
-    });
+    })
 
     const getWeatherIconUrl = (main: string): string => {
         if (main === "Sunny" || main === "Clear") {
-            return "img/2.png";
+            return "img/2.png"
         }
         if (main === "Clouds") {
-            return "img/3.png";
+            return "img/3.png"
         }
         if (main === "Rain" || main === "Drizzle") {
-            return "img/5.png";
+            return "img/5.png"
         }
         if (main === "Snow") {
-            return "img/7.png";
+            return "img/7.png"
         }
         if (main === "Thunderstorm") {
-            return "img/8.png";
+            return "img/8.png"
         }
         if (main === "Mist" || main === "Fog") {
-            return "img/9.png";
+            return "img/9.png"
         }
         if (main === "Smoke") {
-            return "img/10.png";
+            return "img/10.png"
         }
         if (main === "Haze") {
-            return "img/11.png";
+            return "img/11.png"
         }
         if (main === "Dust") {
-            return "img/12.png";
+            return "img/12.png"
         }
         if (main === "Sand") {
-            return "img/13.png";
+            return "img/13.png"
         }
         if (main === "Tornado") {
-            return "img/14.png";
+            return "img/14.png"
         }
-        return "img/3.png";
-    };
+        return "img/3.png"
+    }
 
     const formatDate = (dateString: string): string => {
-        const date = new Date(dateString);
+        const date = new Date(dateString)
         const options: Intl.DateTimeFormatOptions = {
             weekday: "short",
             day: "numeric",
             month: "numeric",
-        };
-        return date.toLocaleDateString("en-US", options);
-    };
+        }
+        return date.toLocaleDateString("en-US", options)
+    }
 
     const formatTime = (dateString: string): string => {
-        const date = new Date(dateString);
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-      
+        const date = new Date(dateString)
+        const hours = date.getHours()
+        const minutes = date.getMinutes()
+
         // Если время равно "24:00", то заменяем на "00:00"
         if (hours === 24 && minutes === 0) {
-            return "00:00";
+            return "00:00"
         }
-      
+
         // Форматируем время обычным способом
-        const formattedHours = hours.toString().padStart(2, "0");
-        const formattedMinutes = minutes.toString().padStart(2, "0");
-        return `${formattedHours}:${formattedMinutes}`;
-    };
+        const formattedHours = hours.toString().padStart(2, "0")
+        const formattedMinutes = minutes.toString().padStart(2, "0")
+        return `${formattedHours}:${formattedMinutes}`
+    }
 
     //test
 
@@ -99,13 +99,21 @@ export function Search() {
                             <li key={index}>
                                 <div className={styles.card}>
                                     <img
-                                        src={getWeatherIconUrl(item.weather?.[0]?.main)}
+                                        src={getWeatherIconUrl(
+                                            item.weather?.[0]?.main
+                                        )}
                                         alt="Icon of weather"
                                     />
-                                    <h3>{Math.ceil(Number(item.main?.temp))}°</h3>
-                                    <h3 className={styles.date}>{formatDate(item.dt_txt)}</h3>
+                                    <h3>
+                                        {Math.ceil(Number(item.main?.temp))}°
+                                    </h3>
+                                    <h3 className={styles.date}>
+                                        {formatDate(item.dt_txt)}
+                                    </h3>
                                     <h3>{data?.city.name}</h3>
-                                    <h3 className={styles.time}>{formatTime(item.dt_txt)}</h3>
+                                    <h3 className={styles.time}>
+                                        {formatTime(item.dt_txt)}
+                                    </h3>
                                 </div>
                             </li>
                         ))}
@@ -113,8 +121,5 @@ export function Search() {
                 </div>
             </div>
         </div>
-    );
+    )
 }
-
-
-
